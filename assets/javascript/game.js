90b316b526e4672a04232d27fa153594ed366ff3
intaglioplate312 <helenelizabeth@me.com>
2017-06-02 14:41:57 -0500

    questions, answers and the beginning of pseudo code illogic

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+var questionOne = 'Who said “Make no small plans” ?'
+var answerOne = ["Richard J. Daley", "Louis Sullivan", "Frederick Law Olmsted", "Al Capone", "Daniel Burnham"]
+					
+var questionTwo = 'Which Firm designed the Chicago Stock Exchange Building from 1893?'
+var answerTwo = ['Jones Lang LaSalle',	'Goldman Sachs',	'Adrian Smith & Gordon Gill', 'Perkins and Will',  'Adler & Sullivan']
+
+var questionFour = "Who was not a member of the Western Association of Architects"
+var answerFour = "John Root	Daniel Burnham	Dankmar Adler	Louis Sullivan	Audie Murphy"
+
+var questionFive = 'Which Frank Lloyd Wright building is in Chicago?'
+var answerTwo = ['Avery Coonley House', 'Bleak House', 'Ennis House', 'Westcott House',	'Robie House']
+
+var questionSix = 'Who heads Studio Gang Architects'	
+var answerTwo = ['Psy', 'James Gang', 'Dalton Gang', 'Olsen Gang', 'Jeanne Gang']
+
+var questionSeven = 'Name the 10th tallest building in Chicago'
+var answerTwo = ['Willis Tower', 'Water Tower'.	'Harbor Point',	'Prudential Plaza',	'The Aqua']
+
+var questionEight = 'What was the first name of the Aon Center'
+var answerTwo = ['BP Center',	'The Blackstone Center', 'Prudential Center', 'Amoco Building',	'Standard Oil Building']
+
+var questionNine = 'Who was the structural engineer for the Sears Tower'	
+var answerTwo = ['John Reeve',	'Bruce Graham',	'Alvah Roebuck', 'Louis Skidmore',	'Fazlur Rahman Khan']
+
+var questionTen = "Which Firm is still in operation today?"
+var answerTwo = ['Holabird & Simonds',	'Laurel & Hardy',	'Adler & Sullivan',	'Martin & Lewis', 	'Holabird & Root']
+
+/*
+// select 5 random questions from selection if time allows
+// populate with answers in random order with attached buttons if not place with ids
+// answer string always [0-3] ==== false [4] === true
+// timer function
+// timer start button
+// at end of timer 
+// run function to add all on clicks matching true and return as "you matched"
+// add all clicks matching false or none retun as "you missed"
+// reset using different quesitons, if time allows
+
+*/
