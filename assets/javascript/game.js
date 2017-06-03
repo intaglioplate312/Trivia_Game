0523a65a17bf4d91e39b17d02636f2dd1d791f76
Your Actual Name <Your Actual Email>
2017-06-02 19:40:29 -0500

    more pseudo illogic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,64 @@
-var questionOne = 'Who said “Make no small plans” ?'
-var answerOne = ["Richard J. Daley", "Louis Sullivan", "Frederick Law Olmsted", "Al Capone", "Daniel Burnham"]
+var questionOne = 'Al Capone said “Make no small plans” ?'
+var answerOne = ["false =Daniel Burnham"]
 					
-var questionTwo = 'Which Firm designed the Chicago Stock Exchange Building from 1893?'
-var answerTwo = ['Jones Lang LaSalle',	'Goldman Sachs',	'Adrian Smith & Gordon Gill', 'Perkins and Will',  'Adler & Sullivan']
+var questionTwo = 'Adler & Sullivan designed the Chicago Stock Exchange Building from 1893?'
+var answerTwo = ['true Adler & Sullivan']
 
-var questionFour = "Who was not a member of the Western Association of Architects"
-var answerFour = "John Root	Daniel Burnham	Dankmar Adler	Louis Sullivan	Audie Murphy"
+var questionFour = "Audie Murphy was a member of the Western Association of Architects"
+var answerFour = "false"
 
-var questionFive = 'Which Frank Lloyd Wright building is in Chicago?'
-var answerTwo = ['Avery Coonley House', 'Bleak House', 'Ennis House', 'Westcott House',	'Robie House']
+var questionFive = 'Avery Coonley House is the only Frank Lloyd Wright house is in Chicago?'
+var answerTwo = [no Robie House]
 
-var questionSix = 'Who heads Studio Gang Architects'	
-var answerTwo = ['Psy', 'James Gang', 'Dalton Gang', 'Olsen Gang', 'Jeanne Gang']
+var questionSix = 'Psy heads Studio Gang Architects'	
+var answerTwo = ['No' 'Jeanne Gang']
 
-var questionSeven = 'Name the 10th tallest building in Chicago'
-var answerTwo = ['Willis Tower', 'Water Tower'.	'Harbor Point',	'Prudential Plaza',	'The Aqua']
+var questionSeven = 'The Aqua 10th tallest building in Chicago'
+var answerTwo = ['true	The Aqua']
 
-var questionEight = 'What was the first name of the Aon Center'
-var answerTwo = ['BP Center',	'The Blackstone Center', 'Prudential Center', 'Amoco Building',	'Standard Oil Building']
+var questionEight = 'the Amoco Building was the original name of the Aon Center'
+var answerTwo = ['false	Standard Oil Building']
 
-var questionNine = 'Who was the structural engineer for the Sears Tower'	
-var answerTwo = ['John Reeve',	'Bruce Graham',	'Alvah Roebuck', 'Louis Skidmore',	'Fazlur Rahman Khan']
+var questionNine = 'Fazlur Rahman Khan was the structural engineer for the Sears Tower'	
+var answerTwo = ['True Fazlur Rahman Khan']
 
-var questionTen = "Which Firm is still in operation today?"
-var answerTwo = ['Holabird & Simonds',	'Laurel & Hardy',	'Adler & Sullivan',	'Martin & Lewis', 	'Holabird & Root']
+var questionTen = "Adler & Sullivan is the oldest Architectural Firm in Chicago?"
+var answerTwo = [False Holabird & Root]
 
 /*
 // select 5 random questions from selection if time allows
 // populate with answers in random order with attached buttons if not place with ids
 // answer string always [0-3] ==== false [4] === true
+<form action="">
+  <input type="radio" name="answer" value="true"> False
+  <input type="radio" name="gender" value="false"> True
+</form>
+
 // timer function
+
+var counter = 90;
+var newYearCountdown = setInterval(function(){
+  console.log(counter);
+  counter--
+  if (counter === 0) {
+    console.log("HAPPY NEW YEAR!!");
+    clearInterval(newYearCountdown);
+  }
+}, 1000);
+
 // timer start button
 // at end of timer 
 // run function to add all on clicks matching true and return as "you matched"
 // add all clicks matching false or none retun as "you missed"
 // reset using different quesitons, if time allows
 
-*/
+* You'll create a trivia form with multiple choice or true/false options (your choice).
+
+* The player will have a limited amount of time to finish the quiz. 
+
+  * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+* Don't let the player pick more than one answer per question.
+
+* Don't forget to include a countdown timer.
+
