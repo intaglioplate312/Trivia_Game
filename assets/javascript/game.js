e077c60c74062132ca57d44e95a722a8de069d5e
intaglioplate312 <helenelizabeth@me.com>
2017-06-05 14:07:35 -0500

    revised timer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,56 @@
 
+
 // You'll create a trivia form with multiple choice or true/false options (your choice).
 //The player will have a limited amount of time to finish the quiz. 
 // The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+//Timer logic
+
+var count=25;
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer(){
+    count=count-1;
+    if (count <= -1){
+     clearInterval(counter);
+     return;
+    }
+
+document.getElementById("time").innerHTML= "you have " + count + " seconds to complete game."; // watch for spelling
+};
 
 
-// timer function
-function startTimer(duration, display) {
-    var timer = duration, seconds;
-    setInterval(function () {
 
-        seconds = parseInt(timer % 60, 10);
-        //ternary annotation -- not sure yet...
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = "You have " + seconds + " to finish the game.";
-        if (--timer === 0) {
-            // not sure why but timer is jerky with out duration??
-            timer = duration;
-            //still need to eleminate NaN
-            timer = clearInterval;
-        }
-    }, 1000);
-}
-//Call timer
-$(".test").click(function () {
-    var timer = 20;
-        display = document.querySelector('#time');
-    startTimer(timer, display);
-   });
+//  Call timer
+//  $(".test").click(function () {
+//  var timer = 20;
+//  display = document.querySelector('#time');
+//  startTimer(timer, display);
+//  });
 
 
 // at end of timer 
-// populate with answers in random order with attached buttons -- wimped out did in HTML/
-// if not place with ids, used class
 //MDN sample to try if time allows
 //$( "input[type=value].tags" ).val(function( index, value ) {
 //return value.trim();
 //});
+
+
+
 var correctAnswer = 0;
 // should work but too simple??
 var incorrectAnswer = 6 - correctAnswer;
 
 //Correct answer function
-function calcscore(){
+function correct(){
     var correctAnswer = 0;
     $(".calc:checked").each(function(){
-        score+=parseInt($(this).val(),10);
+        correctAnswer+=parseInt($(this).val(),10);
     });
-    $("input[name=sum]").val(correctAnswer)
+    $("input[name=sum]").val(correctAnswer) 
 }
 $().ready(function(){
     $(".calc").change(function(){
-        calcscore()
+        correct()
     });
 });
 
