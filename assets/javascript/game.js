263ea6ef00355a7f5f6c4edabc27d0c35f6186d4
intaglioplate312 <helenelizabeth@me.com>
2017-06-05 09:58:00 -0500

    very very basic logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,71 @@
-var questionOne = 'Al Capone said “Make no small plans” ?'
-var answerOne = ["false =Daniel Burnham"]
-					
-var questionTwo = 'Adler & Sullivan designed the Chicago Stock Exchange Building from 1893?'
-var answerTwo = ['true Adler & Sullivan']
 
-var questionFour = "Audie Murphy was a member of the Western Association of Architects"
-var answerFour = "false"
+// You'll create a trivia form with multiple choice or true/false options (your choice).
+//The player will have a limited amount of time to finish the quiz. 
+// The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
 
-var questionFive = 'Avery Coonley House is the only Frank Lloyd Wright house is in Chicago?'
-var answerTwo = [no Robie House]
-
-var questionSix = 'Psy heads Studio Gang Architects'	
-var answerTwo = ['No' 'Jeanne Gang']
-
-var questionSeven = 'The Aqua 10th tallest building in Chicago'
-var answerTwo = ['true	The Aqua']
-
-var questionEight = 'the Amoco Building was the original name of the Aon Center'
-var answerTwo = ['false	Standard Oil Building']
-
-var questionNine = 'Fazlur Rahman Khan was the structural engineer for the Sears Tower'	
-var answerTwo = ['True Fazlur Rahman Khan']
-
-var questionTen = "Adler & Sullivan is the oldest Architectural Firm in Chicago?"
-var answerTwo = [False Holabird & Root]
-
-/*
-// select 5 random questions from selection if time allows
-// populate with answers in random order with attached buttons if not place with ids
-// answer string always [0-3] ==== false [4] === true
-<form action="">
-  <input type="radio" name="answer" value="true"> False
-  <input type="radio" name="gender" value="false"> True
-</form>
 
 // timer function
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(function () {
 
-var counter = 90;
-var newYearCountdown = setInterval(function(){
-  console.log(counter);
-  counter--
-  if (counter === 0) {
-    console.log("HAPPY NEW YEAR!!");
-    clearInterval(newYearCountdown);
-  }
-}, 1000);
+        seconds = parseInt(timer % 60, 10);
+        //ternary annotation -- not sure yet...
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = "You have " + seconds + " to finish the game.";
+        if (--timer === 0) {
+            // not sure why but timer is jerky with out duration??
+            timer = duration;
+            //still need to eleminate NaN
+            timer = clearInterval;
+        }
+    }, 1000);
+}
+//Call timer
+$(".test").click(function () {
+    var timer = 20;
+        display = document.querySelector('#time');
+    startTimer(timer, display);
+   });
 
-// timer start button
+
 // at end of timer 
+// populate with answers in random order with attached buttons -- wimped out did in HTML/
+// if not place with ids, used class
+//MDN sample to try if time allows
+//$( "input[type=value].tags" ).val(function( index, value ) {
+//return value.trim();
+//});
+var correctAnswer = 0;
+// should work but too simple??
+var incorrectAnswer = 6 - correctAnswer;
+
+//Correct answer function
+function calcscore(){
+    var correctAnswer = 0;
+    $(".calc:checked").each(function(){
+        score+=parseInt($(this).val(),10);
+    });
+    $("input[name=sum]").val(correctAnswer)
+}
+$().ready(function(){
+    $(".calc").change(function(){
+        calcscore()
+    });
+});
+
+          
 // run function to add all on clicks matching true and return as "you matched"
 // add all clicks matching false or none retun as "you missed"
 // reset using different quesitons, if time allows
 
-* You'll create a trivia form with multiple choice or true/false options (your choice).
+// You'll create a trivia form with multiple choice or true/false options (your choice).
 
-* The player will have a limited amount of time to finish the quiz. 
+//The player will have a limited amount of time to finish the quiz. 
 
-  * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+// The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
 
-* Don't let the player pick more than one answer per question.
+//Don't let the player pick more than one answer per question.
 
-* Don't forget to include a countdown timer.
+//Don't forget to include a countdown timer.
 
